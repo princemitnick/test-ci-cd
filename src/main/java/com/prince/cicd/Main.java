78991bb4298988fd0fed92eb01b19c8a8e9d7577
Jean Baptiste Prince Stanley Lovensky J <prstanley@pih.org>
2022-03-03 16:14:01 -0500

    Convert to Lamba Expr Project

--- src/main/java/com/prince/cicd/Main.java
+++ src/main/java/com/prince/cicd/Main.java
@@ -1,8 +1,31 @@
 package com.prince.cicd;
 
+import com.prince.cicd.test.lambda.Supplier;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String... args){
 
+        Supplier<String> supplier = () -> "Test CI/CD";
+        System.out.println(supplier.get());
+
+    }
+
+    public static void test(){
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(5);
+        numbers.add(8);
+        numbers.add(9);
+        numbers.add(2);
+        numbers.add(1);
+
+        numbers.forEach((n) -> {
+            if(n % 2 == 0){
+                System.out.println(n);
+            }
+        });
     }
 }
