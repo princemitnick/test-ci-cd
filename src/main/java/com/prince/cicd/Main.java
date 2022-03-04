e1ae3f62ff8e87d355886fe3628a9b8e0dfb018b
Jean Baptiste Prince Stanley Lovensky J <prstanley@pih.org>
2022-03-04 03:50:13 -0500

    Convert to Lamba Expr Project - Test 4-5

--- src/main/java/com/prince/cicd/Main.java
+++ src/main/java/com/prince/cicd/Main.java
@@ -12,11 +12,6 @@ public class Main {
         Supplier<String> supplier = () -> "Test CI/CD";
         System.out.println(supplier.get());
         System.out.println("Test CI/CD -> Lambda Expression : No-1");
-        System.out.println("Test CI/CD -> Lambda Expression : No-2");
-        System.out.println("Test CI/CD -> Lambda Expression : No-3");
-        System.out.println("Test CI/CD -> Lambda Expression : No-4");
-        System.out.println("Test CI/CD -> Lambda Expression : No-5");
-
     }
 
     public static void test(){
@@ -26,6 +21,7 @@ public class Main {
         numbers.add(9);
         numbers.add(2);
         numbers.add(1);
+        numbers.add(4);
 
         numbers.forEach((n) -> {
             if(n % 2 == 0){
